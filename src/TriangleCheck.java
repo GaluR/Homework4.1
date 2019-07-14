5448dc3fa38fa6e54d8ece30ca98b5b4c3a1f202
Bartosz Galinski <galinski.bartosz@gmail.com>
2019-07-14 14:48:27 +0200

    Correct condition in class isRightTriangle.

--- src/TriangleCheck.java
+++ src/TriangleCheck.java
@@ -2,9 +2,9 @@ public class TriangleCheck {
 
     boolean isRightTriangle (Triangle triangle){
 
-            boolean isRectangular1 = !(doMultiply(triangle.side1) + doMultiply(triangle.side2) != doMultiply(triangle.side3));
-            boolean isRectangular2 = !(doMultiply(triangle.side3) + doMultiply(triangle.side2) != doMultiply(triangle.side1));
-            boolean isRectangular3 = !(doMultiply(triangle.side1) + doMultiply(triangle.side3) != doMultiply(triangle.side2));
+            boolean isRectangular1 = (doMultiply(triangle.side1) + doMultiply(triangle.side2) == doMultiply(triangle.side3));
+            boolean isRectangular2 = (doMultiply(triangle.side3) + doMultiply(triangle.side2) == doMultiply(triangle.side1));
+            boolean isRectangular3 = (doMultiply(triangle.side1) + doMultiply(triangle.side3) == doMultiply(triangle.side2));
             return isRectangular1 || isRectangular2 || isRectangular3;
     }
 
