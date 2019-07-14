e97eb96da7cb187e7f7391a8f8579b2e936afa08
Bartosz Galinski <galinski.bartosz@gmail.com>
2019-07-14 14:48:35 +0200

    Correct condition in class isRightTriangle.

--- src/TriangleTest.java
+++ src/TriangleTest.java
@@ -1,6 +1,6 @@
 public class TriangleTest {
     public static void main(String[] args) {
-        Triangle triangle = new Triangle(4, 7, 5);
+        Triangle triangle = new Triangle(4, 5, 3);
         TriangleCheck triangleCheck = new TriangleCheck();
         System.out.println(triangleCheck.isRightTriangle(triangle));
     }
